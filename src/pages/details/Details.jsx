f369ed5f9c79ac6895f280aa01096a389cd6828f
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-04-03 17:44:38 +0200

    Complete cast section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import useAxiosFetch from '../../hooks/useAxiosFetch'
+import Cast from './cast/Cast'
+import DetailsBanner from './detailsBanner/DetailsBanner'
 
 import "./style.scss"
 
 const Details = () => {
+    const { mediaType, id } = useParams()
+    const { data: videos } = useAxiosFetch(`/${mediaType}/${id}/videos`)
+    const { data: credits, isLoading: isCreditsLoading } = useAxiosFetch(`/${mediaType}/${id}/credits`)
+
     return (
-        <div>Details</div>
+        <div>
+            <DetailsBanner video={videos?.results?.[0]} crew={credits?.crew} />
+            <Cast data={credits.cast} isLoading={isCreditsLoading} />
+        </div>
     )
 }
 
