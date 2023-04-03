713114e809f6b4944c7286d55f74442a36d40fd1
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-04-03 11:57:54 +0200

    Complete trending section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import HeroBanner from './heroBanner/HeroBanner'
 
 import "./style.scss"
+import Trending from './trending/Trending'
 
 const Home = () => {
     return (
         <section className="home">
             <HeroBanner />
+            <Trending />
         </section>
     )
 }
